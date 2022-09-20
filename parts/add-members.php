a0df65552b0478485c7b931dcd32455e77d497a6
Stepan <stepanisworking@gmail.com>
2022-09-20 17:50:20 +0300

    improved lists generation

--- parts/add-members.php
+++ parts/add-members.php
@@ -11,11 +11,13 @@ if  ($item['team'] == "Design") {
     $teamColor = "greensea";
 }
 
-$avatarImg = "";
+// $avatarImg = "";
 $hideLettersOnAvatar = "";
+$hideImg = "hide";
 if ($item['avatarlink'] != null) {
-    $avatarImg = '<img src="'. $item['avatarlink'] .'" alt="">';
+    // $avatarImg = '<img src="'. $item['avatarlink'] .'" alt="">';
     $hideLettersOnAvatar = "hide";
+    $hideImg = "";
 }
 
 $firstLetter = mb_substr($item['firstname'],0,1);
@@ -34,15 +36,33 @@ if ($item['donotdisturb'] == 1) {
     $userStatus = "online";
 }
 
-echo 
-'<li class="list-row">
-<div class="list-avatar '. $teamColor .'">
-'. $avatarImg .'
-<span class="'. $hideLettersOnAvatar .'">'. $avatarLetters .'</span>
-</div>
-<span class="list-name">'. $item['firstname'] .' '. $item['lastname'] .'</span>
-<div class="info-list-indicator '. $userStatus .'"></div>
-<span class="info-list-label '. $hideJobLabel .'">'. $item['job'] .'</span>
-</li>';
+// echo 
+// '<li class="list-row">
+
+// <div class="list-avatar '. $teamColor .'">
+
+// '. $avatarImg .'
+
+// <span class="'. $hideLettersOnAvatar .'">'. $avatarLetters .'</span>
+
+// </div>
+
+// <span class="list-name">'. $item['firstname'] .' '. $item['lastname'] .'</span>
+
+// <div class="info-list-indicator '. $userStatus .'"></div>
+
+// <span class="info-list-label '. $hideJobLabel .'">'. $item['job'] .'</span>
+
+// </li>';
 
 ?>
+
+<li class="list-row">
+    <div class="list-avatar <?php echo $teamColor; ?>">
+        <img class="<?php echo $hideImg; ?>" src="<?php echo $item['avatarlink']; ?>" alt="">
+        <span class="<?php echo $hideLettersOnAvatar; ?>"><?php echo $avatarLetters; ?></span>
+    </div>
+    <span class="list-name"><?php echo $item['firstname'] . " " . $item['lastname']; ?></span>
+    <div class="info-list-indicator <?php echo $userStatus; ?>"></div>
+    <span class="info-list-label <?php echo $hideJobLabel; ?>"><?php echo $item['job']; ?></span>
+</li>
--- parts/add-users.php
+++ parts/add-users.php
@@ -11,11 +11,13 @@ if  ($item['team'] == "Design") {
     $teamColor = "greensea";
 }
 
-$avatarImg = "";
+// $avatarImg = "";
 $hideLettersOnAvatar = "";
+$hideImg = "hide";
 if ($item['avatarlink'] != null) {
-    $avatarImg = '<img src="'. $item['avatarlink'] .'" alt="">';
+    // $avatarImg = '<img src="'. $item['avatarlink'] .'" alt="">';
     $hideLettersOnAvatar = "hide";
+    $hideImg = "";
 }
 
 $firstLetter = mb_substr($item['firstname'],0,1);
@@ -27,14 +29,30 @@ if ($item['newmessages'] == 0) {
     $hideLabel = "zero";
 }
 
-echo 
-'<li class="group-row">
-<div class="list-avatar '. $teamColor .'">
-'. $avatarImg .'
-<span class="'. $hideLettersOnAvatar .'">'. $avatarLetters .'</span>
-</div>
-<span class="list-name">'. $item['firstname'] .' '. $item['lastname'] .'</span>
-<span class="list-label '. $hideLabel .'"><div>'. $item['newmessages'] .'</div></span>
-</li>';
+// echo 
+// '<li class="group-row">
+
+// <div class="list-avatar '. $teamColor .'">
+
+// '. $avatarImg .'
+
+// <span class="'. $hideLettersOnAvatar .'">'. $avatarLetters .'</span>
+
+// </div>
+
+// <span class="list-name">'. $item['firstname'] .' '. $item['lastname'] .'</span>
+
+// <span class="list-label '. $hideLabel .'"><div>'. $item['newmessages'] .'</div></span>
+
+// </li>';
 
 ?>
+
+<li class="group-row">
+    <div class="list-avatar <?php echo $teamColor; ?>">
+        <img class="<?php echo $hideImg; ?>" src="<?php echo $item['avatarlink']; ?>" alt="">
+        <span class="<?php echo $hideLettersOnAvatar; ?>"><?php echo $avatarLetters; ?></span>
+    </div>
+    <span class="list-name"><?php echo $item['firstname'] . " " . $item['lastname']; ?></span>
+    <span class="list-label <?php echo $hideLabel; ?>"><div><?php echo $item['newmessages']; ?></div></span>
+</li>
